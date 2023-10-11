762c47f14734382f71bbda4407b4ebda1ec13cec
Bruno Burin <cburin.bruno@gmail.com>
2023-10-10 23:30:49 -0500

    update independent set based model to solve minimum biclique cover problem

--- src/independent_set.py
+++ src/independent_set.py
@@ -4,9 +4,8 @@ from gurobipy import GRB
 import networkx as nx
 from pandas import DataFrame
 
-from bc_bounds import find_bc_lower_bound, find_bc_upper_bound, LBComputeMethod, UBComputeMethod
+from bc_bounds import find_bc_upper_bound, UBComputeMethod
 from util import get_graphs_in_store, chronometer
-import vertex_cover
 
 
 def solve(G):    # define model
@@ -41,10 +40,9 @@ def get_set_of_maximal_independent_sets(g: nx.Graph):
 
 
 def solve_bc(g: nx.Graph):
-    # sets and lists are unhashable and tuples could not index variables, thus indep_sets stores int keys to index
-    # decision variables and the actual sets as values
-    indep_sets = {i: tuple(s) for i, s in enumerate(get_set_of_maximal_independent_sets(g))}
-    lb = int(find_bc_lower_bound(g, method=LBComputeMethod.INDEPENDENT_EDGES))
+    # use frozen sets so they are hashable
+    indep_sets = frozenset(frozenset(indep_set) for indep_set in get_set_of_maximal_independent_sets(g))
+    lb = 1  # independent edges method doesn't work
     ub = int(find_bc_upper_bound(g, method=UBComputeMethod.VERTEX))
     dg = nx.DiGraph(g)
     bicliques = range(ub)
@@ -52,27 +50,35 @@ def solve_bc(g: nx.Graph):
     # create model
     m = gp.Model()
     # define variables
-    z = m.addVars(bicliques, vtype=GRB.BINARY, name='z')
-    w = m.addVars(indep_sets.keys(), bicliques, range(2), vtype=GRB.BINARY, name='w')
-    x = m.addVars(g.nodes, bicliques, range(2), vtype=GRB.BINARY, name='x')
-    p = m.addVars(g.nodes, bicliques, range(2), vtype=GRB.BINARY, name='p')
-    y = m.addVars(dg.edges, bicliques, vtype=GRB.BINARY, name='y')
+    w = m.addVars(indep_sets, bicliques, range(2), vtype=GRB.BINARY, name='w')
+    x = m.addVars(g.nodes, bicliques, range(2), vtype=GRB.CONTINUOUS, lb=0, ub=1, name='x')  # relaxed
+    p = m.addVars(g.nodes, bicliques, range(2), vtype=GRB.CONTINUOUS, lb=0, ub=1, name='p')  # relaxed
+    y = m.addVars(dg.edges, bicliques, vtype=GRB.CONTINUOUS, lb=0, ub=1, name='y')           # relaxed
+    z = m.addVars(bicliques, vtype=GRB.CONTINUOUS, lb=0, ub=1, name='z')                     # relaxed
     # objective function
     m.setObjective(gp.quicksum(z), GRB.MINIMIZE)
-    # constraints
-    m.addConstrs(w[si, b, 0] + w[si, b, 1] <= 1 for si in indep_sets.keys() for b in bicliques)
-    m.addConstrs(w[si, b, d] <= x[u, b, d]
-                 for si, s in indep_sets.items() for u in s for b in bicliques for d in range(2))
-    m.addConstrs(x[u, b, d] <= gp.quicksum(w[si, b, d] for si, s in indep_sets.items() if u in s)
+    # independent set partition constraints
+    m.addConstrs(w[s, b, 0] + w[s, b, 1] <= 1 for s in indep_sets for b in bicliques)
+    m.addConstrs(w[s, b, d] <= x[u, b, d]
+                 for s in indep_sets for u in s for b in bicliques for d in range(2))
+    m.addConstrs(x[u, b, d] <= gp.quicksum(w[s, b, d] for s in indep_sets if u in s)
                  for u in g.nodes for b in bicliques for d in range(2))
-    m.addConstrs(2*p[u, b, d] <= x[u, b, d] - x[u, b, 1-d] + 1 for u in g.nodes for b in bicliques for d in range(2))
-    m.addConstrs(2*y[u, v, b] <= p[u, b, 0] + p[v, b, 1] for u, v in dg.edges for b in bicliques)
-    m.addConstrs(p[u, b, 0] + p[v, b, 1] - 1 <= y[u, v, b] for u, v in dg.edges for b in bicliques)
+    # p[u, b, d] = x[u, b, d] * (1 - x[u, b, 1-d])
+    m.addConstrs(p[u, b, d] <= x[u, b, d] for u in g.nodes for b in bicliques for d in range(2))
+    m.addConstrs(p[u, b, d] <= 1 - x[u, b, 1-d] for u in g.nodes for b in bicliques for d in range(2))
+    m.addConstrs(p[u, b, d] >= x[u, b, d] - x[u, b, 1-d] for u in g.nodes for b in bicliques for d in range(2))
+    # y[u, v, b] = p[u, b, 0] * p[v, b, 1]
+    m.addConstrs(y[u, v, b] <= p[u, b, 0] for u, v in dg.edges for b in bicliques)
+    m.addConstrs(y[u, v, b] <= p[v, b, 1] for u, v in dg.edges for b in bicliques)
+    m.addConstrs(y[u, v, b] >= p[u, b, 0] + p[v, b, 1] - 1 for u, v in dg.edges for b in bicliques)
+    # cover constraints
     m.addConstrs(gp.quicksum(y[u, v, b] + y[v, u, b] for b in bicliques) >= 1 for u, v in g.edges)
-    m.addConstrs(p[u, b, 0] + p[v, b, 1] <= 1 for u, v in nx.complement(dg).edges for b in bicliques)
-    m.addConstrs(z[b + 1] <= z[b] for b in range(ub-1))
-    m.addConstrs(z[b] == 1 for b in range(lb))
     m.addConstrs(y[u, v, b] <= z[b] for u, v in dg.edges for b in bicliques)
+    m.addConstrs(p[u, b, 0] + p[v, b, 1] <= 1 for u, v in nx.complement(dg).edges for b in bicliques)
+    # symmetry break
+    m.addConstrs(z[b + 1] <= z[b] for b in range(ub-1))
+    # lower bound
+    m.addConstrs(z[b] == 1 for b in range(lb))
 
     m.optimize()
 
@@ -80,16 +86,15 @@ def solve_bc(g: nx.Graph):
     df_bicliques = DataFrame(
         {b: [False] * len(g.edges) for b in bicliques if z[b].X == 1}, index=[str(e) for e in g.edges])
     for e in g.edges:
+        u, v = e
         for b in bicliques:
-            if z[b].X == 1:
-                u, v = e
-                if y[u, v, b].X == 1 or y[v, u, b].X == 1:
-                    df_bicliques.loc[str(e), b] = True
+            if z[b].X == 1 and (y[u, v, b].X == 1 or y[v, u, b].X == 1):
+                df_bicliques.loc[str(e), b] = True
 
     return m.objVal, df_bicliques
 
 
-def print_independent_sets(g: nx.Graph, max_indep_sets: int = None):
+def print_independent_sets(g: nx.Graph, g_name, max_indep_sets: int = None):
     max_independent_sets, compute_time = chronometer(lambda x: set(get_set_of_maximal_independent_sets(x)), g)
     print(f'Graph:', g_name)
     print(f'Nodes:', len(g.nodes))
