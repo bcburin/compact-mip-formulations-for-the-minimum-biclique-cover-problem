ed9fa46834a8f8077f3e0a809b166e62a45dae9e
Bruno Burin <cburin.bruno@gmail.com>
2023-10-24 17:07:43 -0500

    refactor models

---
+++ src/base_model.py
@@ -0,0 +1,212 @@
+from abc import ABC, abstractmethod
+from functools import cache
+from os import path
+from typing import TextIO, Iterable
+
+import gurobipy as gp
+import networkx as nx
+import pandas as pd
+from gurobipy import GRB
+from pandas import DataFrame
+
+from src.bc_bounds import LBComputeMethod, UBComputeMethod, find_bc_upper_bound
+from src.independent_set import get_set_of_maximal_independent_sets
+
+
+class MBCModel(ABC):
+
+    def __init__(
+             self,
+             g: nx.Graph,
+             g_name: str,
+             lb_method: LBComputeMethod | None = None,
+             ub_method: UBComputeMethod = UBComputeMethod.VERTEX,
+             time_limit: int = None,
+             log_to_console: bool = True,
+             dir_logs: str = None):
+        self.g = g
+        self.g_name = g_name
+        self.lb_method = lb_method
+        self.ub_method = ub_method
+        self._logging = bool(dir_logs)
+        self.time_limit = time_limit
+        self.log_to_console = log_to_console
+        # model
+        self.m = gp.Model()
+        self._add_variables()
+        self._set_objective()
+        self._add_constraints()
+        # create log files
+        if self._logging:
+            model_graph_name = self.name() + '_' + self.g_name
+            self.log_grb = open(path.join(dir_logs, model_graph_name + '_gurobi.log'), 'w+')
+            self.log_res = open(path.join(dir_logs, model_graph_name + '_result.log'), 'w+')
+            self._write_headers(self.log_res)
+        # parameters
+        self._set_parameters()
+        # initial state
+        self._solved = False
+
+    def _close_files(self):
+        if self._logging:
+            self.log_grb.close()
+            self.log_res.close()
+
+    def _set_parameters(self):
+        if self.time_limit is not None:
+            self.m.Params.TimeLimit = self.time_limit
+        if not self.log_to_console:
+            self.m.Params.LogToConsole = 0
+        if self._logging:
+            self.m.Params.LogFile = self.log_grb.name
+
+    def _write_headers(self, log: TextIO):
+        log.write(f'GRAPH: {self.g_name}\n')
+        log.write(f'NODES: {len(self.g.nodes)}\n')
+        log.write(f'EDGES: {len(self.g.edges)}\n\n')
+
+    @abstractmethod
+    def _add_variables(self):
+        ...
+
+    @abstractmethod
+    def _add_constraints(self):
+        ...
+
+    @abstractmethod
+    def _set_objective(self):
+        ...
+
+    @classmethod
+    @abstractmethod
+    def name(cls) -> str:
+        ...
+
+    def _pre_solve(self):
+        # add lower bound constraints
+        if self.lower_bound:
+            for b in range(self.lower_bound):
+                self.z[b].lb = 1
+
+    def _post_solve(self):
+        # check and log solution
+        if not self._logging:
+            return
+        if self.m.status == GRB.OPTIMAL:
+            # construct dataframe with bicliques
+            df_bicliques = self.get_bicliques()
+            # check solution
+            self.log_res.write(f'IS BICLIQUE COVER? {"Y" if self.is_biclique_cover(df_bicliques) else "N"}')
+            # save solution
+            self.log_res.write(f'\nBICLIQUE COVER DATAFRAME:\n{df_bicliques}\n\n')
+            self.log_grb.close()
+            self.log_res.close()
+        elif self.m.status == GRB.TIME_LIMIT:
+            self.log_res.write(f'\nMODEL REACHED TIME LIMIT OF {self.time_limit} SECONDS\n\n')
+        else:
+            self.log_res.write(f'\nTHERE WAS AN UNEXPECTED ERROR\n\n')
+        self._close_files()
+
+    @cache
+    def get_bicliques(self) -> pd.DataFrame:
+        """
+        This method builds a Pandas dataframe containing the solution of a biclique cover. For this to work for
+        a model, it is enough that said model define z variables representing bicliques and y variables representing
+        whether an arc belongs to a biclique.
+
+        :return: a binary dataframe, where each column is a biclique and each line is indexed by a node. Each cell can
+                 hold the values 0, 1, or 2. If the node does not belong to the biclique, the value is zero. If the node
+                 belongs to the first partition of the biclique, the value is one; likewise, if the node belongs to the
+                 second partition, the value of the cell is 2.
+        :raises RuntimeError: if the model was not solved yet or if it does not define z or y variables.
+        """
+        if not self._solved:
+            raise RuntimeError('Model not solved yet.')
+        if not hasattr(self, 'z') or not hasattr(self, 'y'):
+            raise RuntimeError('Model does not define z or y variables. They are required to build the '
+                               'biclique dataframe.')
+
+        df_bicliques = DataFrame(
+            {b: [0] * len(self.g.nodes) for b in self.bicliques if self.z[b].X == 1}, index=[u for u in self.g.nodes])
+        for e in self.g.edges:
+            u, v = e
+            for b in self.bicliques:
+                if self.z[b].X == 1:
+                    if self.y[u, v, b].X == 1:
+                        df_bicliques.loc[u, b] = 1
+                        df_bicliques.loc[v, b] = 2
+                    if self.y[v, u, b].X == 1:
+                        df_bicliques.loc[v, b] = 1
+                        df_bicliques.loc[u, b] = 2
+        return df_bicliques
+
+    def is_biclique_cover(self, df: pd.DataFrame) -> bool:
+        """
+        This method determines whether the solution to the minimum biclique cover problem found by a model is actually
+        a valid biclique cover.
+
+        :param df: dataframe containing information of each biclique in the biclique cover, as found by the model and
+                   generated by the function get_biclique_cover_dataframe.
+        :return: True if the dataframe represent a valid biclique cover; False otherwise.
+        """
+        # check if it is a cover
+        if not df.any(axis=1).all():
+            return False
+        # check if each column is, indeed, a biclique
+        for biclique in df.columns:
+            partition1: list[int] = df[df[biclique] == 1].index.tolist()
+            partition2: list[int] = df[df[biclique] == 2].index.tolist()
+            if any(not self.g.has_edge(u, v) for u in partition1 for v in partition2):
+                return False
+        return True
+
+    @property
+    @cache
+    def directed(self) -> nx.DiGraph:
+        return nx.DiGraph(self.g)
+
+    @property
+    @cache
+    def complement(self) -> nx.Graph:
+        return nx.complement(self.g)
+
+    @property
+    @cache
+    def directed_complement(self) -> nx.Graph:
+        return nx.complement(self.directed)
+
+    @property
+    @cache
+    def lower_bound(self) -> int:
+        return 1
+
+    @property
+    @cache
+    def upper_bound(self) -> int:
+        return int(find_bc_upper_bound(self.g, self.ub_method))
+
+    @property
+    def bicliques(self) -> Iterable:
+        return range(self.upper_bound)
+
+    def solve(self):
+        # custom pre-solve
+        self._pre_solve()
+        # optimization process
+        self.m.optimize()
+        self._solved = True
+        # custom post-solve with default implementation
+        self._post_solve()
+        # return obj val
+        return self.m.objVal
+
+
+class IndepSetProvider:
+
+    def __init__(self, g: nx.Graph):
+        self.g = g
+
+    @property
+    @cache
+    def indep_sets(self) -> frozenset[frozenset[int]]:
+        return frozenset(frozenset(indep_set) for indep_set in get_set_of_maximal_independent_sets(self.g))
--- src/model.py
+++ src/model.py
@@ -1,205 +1,174 @@
 from datetime import datetime
 from os import path, getcwd, pardir, mkdir
-from typing import Iterable, TextIO
+from typing import Type
 
 import gurobipy as gp
 import networkx as nx
-import pandas as pd
 from gurobipy import GRB
-from pandas import DataFrame
 
-from src.bc_bounds import find_bc_upper_bound, UBComputeMethod
-from src.independent_set import get_set_of_maximal_independent_sets
+from src.base_model import MBCModel, IndepSetProvider
 from src.util import get_graphs_in_store, GraphReport
 
 
-def solve_bc_v1(g: nx.Graph, log_gb: TextIO = None, log_res: TextIO = None, time_limit: int = None):
-    lb = 1
-    ub = int(find_bc_upper_bound(g, method=UBComputeMethod.VERTEX))
-    dg = nx.DiGraph(g)
-    cg = nx.complement(dg)
-    bicliques = range(ub)
+class ModelV1(MBCModel):
 
-    # create model
-    m = gp.Model()
-    # set parameters
-    if time_limit is not None:
-        m.Params.TimeLimit = time_limit
-    if log_gb is not None:
-        m.Params.LogFile = log_gb.name
-        # m.Params.LogToConsole = 0
-    # define variables
-    z = m.addVars(bicliques, vtype=GRB.BINARY, name="z")
-    x = m.addVars(g.nodes, bicliques, range(2), vtype=GRB.BINARY, name="x")
-    y = m.addVars(dg.edges, bicliques, lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name="y")
-    # define objective function
-    m.setObjective(gp.quicksum(z), sense=GRB.MINIMIZE)
-    # symmetry-break constraints
-    m.addConstrs(z[b] >= z[b + 1] for b in range(ub - 1))
-    # coupling constraints
-    m.addConstrs(x[v, b, i] <= z[b] for v in g.nodes for b in bicliques for i in range(2))
-    # covering constraints
-    m.addConstrs(gp.quicksum(y[u, v, b] + y[v, u, b] for b in bicliques) >= 1 for u, v in g.edges)
-    # node assignment constraints
-    m.addConstrs(x[v, b, 0] + x[v, b, 1] <= z[b] for v in g.nodes for b in bicliques)
-    # node and edge constraints
-    for u, v in dg.edges:
-        m.addConstrs(y[u, v, b] <= x[u, b, 0] for b in bicliques)
-        m.addConstrs(y[u, v, b] <= x[v, b, 1] for b in bicliques)
-        m.addConstrs(y[u, v, b] >= x[u, b, 0] + x[v, b, 1] - z[b] for b in bicliques)
-    for u, v in cg.edges:
-        m.addConstrs(x[u, b, 0] + x[v, b, 1] <= z[b] for b in bicliques)
-    # lower bound
-    for b in range(lb):
-        z[b].lb = 1
+    def _add_variables(self):
+        self.z = self.m.addVars(self.bicliques, vtype=GRB.BINARY, name="z")
+        self.x = self.m.addVars(self.g.nodes, self.bicliques, range(2), vtype=GRB.BINARY, name="x")
+        self.y = self.m.addVars(
+            self.directed.edges, self.bicliques, lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name="y")
 
-    m.optimize()
+    def _add_constraints(self):
+        # set aliases
+        m = self.m
+        x = self.x
+        y = self.y
+        z = self.z
+        bicliques = self.bicliques
 
-    if m.status == GRB.OPTIMAL:
-        # construct dataframe with bicliques
-        df_bicliques = get_biclique_cover_dataframe(g=g, bicliques=bicliques, z=z, y=y)
-        # check solution
-        log_res.write(f'IS BICLIQUE COVER? {"Y" if is_biclique_cover(g, df_bicliques) else "N"}')
-        # save solution
-        log_res.write(f'\nBICLIQUE COVER DATAFRAME:\n{df_bicliques}\n\n')
+        # symmetry-break constraints
+        m.addConstrs(z[b] >= z[b + 1] for b in range(self.upper_bound - 1))
+        # coupling constraints
+        m.addConstrs(x[v, b, i] <= z[b] for v in self.g.nodes for b in bicliques for i in range(2))
+        # covering constraints
+        m.addConstrs(gp.quicksum(y[u, v, b] + y[v, u, b] for b in bicliques) >= 1 for u, v in self.g.edges)
+        # node assignment constraints
+        m.addConstrs(x[v, b, 0] + x[v, b, 1] <= z[b] for v in self.g.nodes for b in bicliques)
+        # node and edge constraints
+        for u, v in self.directed.edges:
+            m.addConstrs(y[u, v, b] <= x[u, b, 0] for b in bicliques)
+            m.addConstrs(y[u, v, b] <= x[v, b, 1] for b in bicliques)
+            m.addConstrs(y[u, v, b] >= x[u, b, 0] + x[v, b, 1] - z[b] for b in bicliques)
+        for u, v in self.directed_complement.edges:
+            m.addConstrs(x[u, b, 0] + x[v, b, 1] <= z[b] for b in bicliques)
 
-        return m.objVal
+    def _set_objective(self):
+        self.m.setObjective(gp.quicksum(self.z), sense=GRB.MINIMIZE)
 
-    if m.status == GRB.TIME_LIMIT:
-        log_res.write(f'\nMODEL REACHED TIME LIMIT OF {time_limit} SECONDS\n')
-    else:
-        log_res.write(f'\nTHERE WAS AN UNEXPECTED ERROR\n')
+    @classmethod
+    def name(cls) -> str:
+        return 'v1'
 
 
-def solve_bc_v2(g: nx.Graph, use_relaxation: bool = True, log_gb: TextIO = None, log_res: TextIO = None,
-                time_limit: int = None):
-    # use frozen sets so the independent sets are hashable
-    indep_sets = frozenset(frozenset(indep_set) for indep_set in get_set_of_maximal_independent_sets(g))
-    lb = 1
-    ub = int(find_bc_upper_bound(g, method=UBComputeMethod.VERTEX))
-    dg = nx.DiGraph(g)
-    bicliques = range(ub)
+class ModelV2(MBCModel, IndepSetProvider):
 
-    # create model
-    m = gp.Model()
-    # set parameters
-    if time_limit is not None:
-        m.Params.TimeLimit = time_limit
-    if log_gb is not None:
-        m.Params.LogFile = log_gb.name
-        # m.Params.LogToConsole = 0
-    # define variables
-    w = m.addVars(indep_sets, bicliques, range(2), vtype=GRB.BINARY, name='w')
-    x = m.addVars(g.nodes, bicliques, range(2),
-                  vtype=GRB.CONTINUOUS if use_relaxation else GRB.INTEGER, lb=0, ub=1, name='x')
-    p = m.addVars(g.nodes, bicliques, range(2),
-                  vtype=GRB.CONTINUOUS if use_relaxation else GRB.INTEGER, lb=0, ub=1, name='p')
-    y = m.addVars(dg.edges, bicliques,
-                  vtype=GRB.CONTINUOUS if use_relaxation else GRB.INTEGER, lb=0, ub=1, name='y')
-    z = m.addVars(bicliques,
-                  vtype=GRB.CONTINUOUS if use_relaxation else GRB.INTEGER, lb=0, ub=1, name='z')
-    # objective function
-    m.setObjective(gp.quicksum(z), GRB.MINIMIZE)
-    # independent set partition constraints
-    m.addConstrs(w[s, b, 0] + w[s, b, 1] <= 1 for s in indep_sets for b in bicliques)
-    m.addConstrs(w[s, b, d] <= x[u, b, d]
-                 for s in indep_sets for u in s for b in bicliques for d in range(2))
-    m.addConstrs(x[u, b, d] <= gp.quicksum(w[s, b, d] for s in indep_sets if u in s)
-                 for u in g.nodes for b in bicliques for d in range(2))
-    # p[u, b, d] = x[u, b, d] * (1 - x[u, b, 1-d])
-    m.addConstrs(p[u, b, d] <= x[u, b, d] for u in g.nodes for b in bicliques for d in range(2))
-    m.addConstrs(p[u, b, d] <= 1 - x[u, b, 1-d] for u in g.nodes for b in bicliques for d in range(2))
-    m.addConstrs(p[u, b, d] >= x[u, b, d] - x[u, b, 1-d] for u in g.nodes for b in bicliques for d in range(2))
-    # y[u, v, b] = p[u, b, 0] * p[v, b, 1]
-    m.addConstrs(y[u, v, b] <= p[u, b, 0] for u, v in dg.edges for b in bicliques)
-    m.addConstrs(y[u, v, b] <= p[v, b, 1] for u, v in dg.edges for b in bicliques)
-    m.addConstrs(y[u, v, b] >= p[u, b, 0] + p[v, b, 1] - 1 for u, v in dg.edges for b in bicliques)
-    # cover constraints
-    m.addConstrs(gp.quicksum(y[u, v, b] + y[v, u, b] for b in bicliques) >= 1 for u, v in g.edges)
-    m.addConstrs(y[u, v, b] <= z[b] for u, v in dg.edges for b in bicliques)
-    m.addConstrs(p[u, b, 0] + p[v, b, 1] <= 1 for u, v in nx.complement(dg).edges for b in bicliques)
-    # symmetry break
-    m.addConstrs(z[b + 1] <= z[b] for b in range(ub-1))
-    # lower bound
-    for b in range(lb):
-        z[b].lb = 1
+    def __init__(self, g: nx.Graph, g_name: str, use_relaxation: bool = True, **kwargs):
+        self.use_relaxation = use_relaxation
+        MBCModel.__init__(self, g, g_name, **kwargs)
+        IndepSetProvider.__init__(self, g)
 
-    m.optimize()
+    def _add_variables(self):
+        bicliques = self.bicliques
 
-    if m.status == GRB.OPTIMAL:
-        # construct dataframe with bicliques
-        df_bicliques = get_biclique_cover_dataframe(g=g, bicliques=bicliques, z=z, y=y)
-        # check solution
-        log_res.write(f'\nIS BICLIQUE COVER? {"Y" if is_biclique_cover(g, df_bicliques) else "N"}\n')
-        # save solution
-        log_res.write(f'\nBICLIQUE COVER DATAFRAME:\n{df_bicliques}\n\n')
+        self.w = self.m.addVars(
+            self.indep_sets, bicliques, range(2), vtype=GRB.BINARY, name='w')
+        self.x = self.m.addVars(
+            self.g.nodes, bicliques, range(2), vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER,
+            lb=0, ub=1, name='x')
+        self.p = self.m.addVars(
+            self.g.nodes, bicliques, range(2), vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER,
+            lb=0, ub=1, name='p')
+        self.y = self.m.addVars(
+            self.directed.edges, bicliques, vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER,
+            lb=0, ub=1, name='y')
+        self.z = self.m.addVars(
+            bicliques, vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER, lb=0, ub=1, name='z')
 
-        return m.objVal
+    def _add_constraints(self):
+        # set aliases
+        g = self.g
+        m = self.m
+        w = self.w
+        x = self.x
+        p = self.p
+        y = self.y
+        z = self.z
+        bicliques = self.bicliques
 
-    if m.status == GRB.TIME_LIMIT:
-        log_res.write(f'\nMODEL REACHED TIME LIMIT OF {time_limit} SECONDS\n')
-    else:
-        log_res.write(f'\nTHERE WAS AN UNEXPECTED ERROR\n')
+        # independent set partition constraints
+        m.addConstrs(w[s, b, 0] + w[s, b, 1] <= 1 for s in self.indep_sets for b in bicliques)
+        m.addConstrs(w[s, b, d] <= x[u, b, d]
+                     for s in self.indep_sets for u in s for b in bicliques for d in range(2))
+        m.addConstrs(x[u, b, d] <= gp.quicksum(w[s, b, d] for s in self.indep_sets if u in s)
+                     for u in g.nodes for b in bicliques for d in range(2))
+        # p[u, b, d] = x[u, b, d] * (1 - x[u, b, 1-d])
+        m.addConstrs(p[u, b, d] <= x[u, b, d] for u in g.nodes for b in bicliques for d in range(2))
+        m.addConstrs(p[u, b, d] <= 1 - x[u, b, 1 - d] for u in g.nodes for b in bicliques for d in range(2))
+        m.addConstrs(p[u, b, d] >= x[u, b, d] - x[u, b, 1 - d] for u in g.nodes for b in bicliques for d in range(2))
+        # y[u, v, b] = p[u, b, 0] * p[v, b, 1]
+        m.addConstrs(y[u, v, b] <= p[u, b, 0] for u, v in self.directed.edges for b in bicliques)
+        m.addConstrs(y[u, v, b] <= p[v, b, 1] for u, v in self.directed.edges for b in bicliques)
+        m.addConstrs(y[u, v, b] >= p[u, b, 0] + p[v, b, 1] - 1 for u, v in self.directed.edges for b in bicliques)
+        # cover constraints
+        m.addConstrs(gp.quicksum(y[u, v, b] + y[v, u, b] for b in bicliques) >= 1 for u, v in g.edges)
+        m.addConstrs(y[u, v, b] <= z[b] for u, v in self.directed.edges for b in bicliques)
+        m.addConstrs(p[u, b, 0] + p[v, b, 1] <= 1 for u, v in nx.complement(self.directed).edges for b in bicliques)
+        # symmetry break
+        m.addConstrs(z[b + 1] <= z[b] for b in range(self.upper_bound - 1))
+
+    def _set_objective(self):
+        self.m.setObjective(gp.quicksum(self.z), sense=GRB.MINIMIZE)
+
+    @classmethod
+    def name(cls) -> str:
+        return 'v2-0'
 
 
-def get_biclique_cover_dataframe(g: nx.Graph, bicliques: Iterable, z: gp.Var, y: gp.Var) -> pd.DataFrame:
-    """
-    This function builds a Pandas dataframe containing the solution of a biclique cover. For this to work for
-    a model, it is enough that said model define z variables representing bicliques and y variables representing
-    whether an arc belongs to a biclique.
+class ModelV21(MBCModel, IndepSetProvider):
 
-    :param g: input networkx graph.
-    :param bicliques: iterable of the type range(ub), where ub is the upper bound of the number of bicliques,
-                      as used in the implementing model.
-    :param z: gurobipy variables representing whether a biclique is selected.
-    :param y: gurobipy variables representing whether an arc is part of a selected biclique.
-    :return: a binary dataframe, where each column is a biclique and each line is indexed by a node. Each cell can
-             hold the values 0, 1, or 2. If the node does not belong to the biclique, the value is zero. If the node
-             belongs to the first partition of the biclique, the value is one; likewise, if the node belongs to the
-             second partition, the value of the cell is 2.
-    """
-    df_bicliques = DataFrame(
-        {b: [0] * len(g.nodes) for b in bicliques if z[b].X == 1}, index=[u for u in g.nodes])
-    for e in g.edges:
-        u, v = e
-        for b in bicliques:
-            if z[b].X == 1:
-                if y[u, v, b].X == 1:
-                    df_bicliques.loc[u, b] = 1
-                    df_bicliques.loc[v, b] = 2
-                if y[v, u, b].X == 1:
-                    df_bicliques.loc[v, b] = 1
-                    df_bicliques.loc[u, b] = 2
-    return df_bicliques
+    def __init__(self, g: nx.Graph, g_name: str, use_relaxation: bool = True, **kwargs):
+        self.use_relaxation = use_relaxation
+        MBCModel.__init__(self, g, g_name, **kwargs)
+        IndepSetProvider.__init__(self, g)
+
+    def _add_variables(self):
+        bicliques = self.bicliques
+
+        self.w = self.m.addVars(self.indep_sets, bicliques, range(2), vtype=GRB.BINARY, name='w')
+        self.x = self.m.addVars(
+            self.g.nodes, bicliques, range(2),
+            vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER, lb=0, ub=1, name='x')
+        self.y = self.m.addVars(
+            self.directed.edges, bicliques,
+            vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER, lb=0, ub=1, name='y')
+        self.z = self.m.addVars(
+            bicliques,
+            vtype=GRB.CONTINUOUS if self.use_relaxation else GRB.INTEGER, lb=0, ub=1, name='z')
+
+    def _add_constraints(self):
+        # set aliases
+        g, m, w, x, y, z, bicliques, indep_sets, directed, upper_bound = (
+            self.g, self.m, self.w, self.x, self.y, self.z, self.bicliques,
+            self.indep_sets, self.directed, self.upper_bound
+        )
+
+        # independent set partition constraints
+        m.addConstrs(w[s, b, 0] + w[s, b, 1] <= 1 for s in self.indep_sets for b in bicliques)
+        m.addConstrs(w[s, b, d] <= x[u, b, d]
+                     for s in self.indep_sets for u in s for b in bicliques for d in range(2))
+        m.addConstrs(x[u, b, d] <= gp.quicksum(w[s, b, d] for s in self.indep_sets if u in s)
+                     for u in g.nodes for b in bicliques for d in range(2))
+        # y[u, v, b] = x[u, b, 0] ^ x[v, b, 1]
+        m.addConstrs(y[u, v, b] <= x[u, b, 0] + x[v, b, 1] for u, v in self.directed.edges for b in bicliques)
+        m.addConstrs(y[u, v, b] <= 2 - x[u, b, 0] - x[v, b, 1] for u, v in self.directed.edges for b in bicliques)
+        m.addConstrs(y[u, v, b] >= x[u, b, 0] - x[v, b, 1] for u, v in self.directed.edges for b in bicliques)
+        m.addConstrs(y[u, v, b] >= x[v, b, 1] - x[u, b, 0] for u, v in self.directed.edges for b in bicliques)
+        # cover constraints
+        m.addConstrs(gp.quicksum(y[u, v, b] + y[v, u, b] for b in bicliques) >= 1 for u, v in g.edges)
+        m.addConstrs(y[u, v, b] <= z[b] for u, v in self.directed.edges for b in bicliques)
+        # symmetry break
+        m.addConstrs(z[b + 1] <= z[b] for b in range(self.upper_bound - 1))
+
+    def _set_objective(self):
+        self.m.setObjective(gp.quicksum(self.z), sense=GRB.MINIMIZE)
+
+    @classmethod
+    def name(cls) -> str:
+        return 'v2-1'
 
 
-def is_biclique_cover(g: nx.Graph, df: pd.DataFrame) -> bool:
-    """
-    This function determines whether the solution to the minimum biclique cover problem found by a model is actually
-    a valid biclique cover.
-
-    :param g: original graph used by the model to find the solution.
-    :param df: dataframe containing information of each biclique in the biclique cover, as found by the model and
-               generated by the function get_biclique_cover_dataframe.
-    :return: True if the dataframe represent a valid biclique cover; False otherwise.
-    """
-    # check if it is a cover
-    if not df.any(axis=1).all():
-        return False
-    # check if each column is, indeed, a biclique
-    for biclique in df.columns:
-        partition1: list[int] = df[df[biclique] == 1].index.tolist()
-        partition2: list[int] = df[df[biclique] == 2].index.tolist()
-        if any(not g.has_edge(u, v) for u in partition1 for v in partition2):
-            return False
-    return True
-
-
-def create_and_save_model_comparison_report(report_name: str, **kwargs):
-    # define constant strings
-    model_v1 = 'model_v1'
-    model_v2 = 'model_v2'
-    # open log files
+def create_and_save_model_comparison_report(report_name: str, model_clss: list[Type[MBCModel]], **kwargs):
+    # get log directory
     dir_parent = path.abspath(path.join(getcwd(), pardir))
     dir_logs = path.join(dir_parent, 'logs')
     if not path.isdir(dir_logs):
@@ -207,37 +176,19 @@ def create_and_save_model_comparison_report(report_name: str, **kwargs):
     ts = int(datetime.now().timestamp())
     dir_ts_logs = path.join(dir_logs, report_name + '-' + str(ts))
     mkdir(dir_ts_logs)
-    log_gb_v1 = open(path.join(dir_ts_logs, model_v1 + '_gb' + '.log'), 'w+')
-    log_gb_v2 = open(path.join(dir_ts_logs, model_v2 + '_gb' + '.log'), 'w+')
-    log_res_v1 = open(path.join(dir_ts_logs, model_v1 + '_res' + '.log'), 'w+')
-    log_res_v2 = open(path.join(dir_ts_logs, model_v2 + '_res' + '.log'), 'w+')
-
-    def write_header(log: TextIO, g: nx.Graph, g_name: str):
-        log.write('\n' + '-' * 80 + '\n')
-        log.write(f'GRAPH: {g_name}\n')
-        log.write(f'NODES: {len(g.nodes)}\n')
-        log.write(f'EDGES: {len(g.edges)}\n\n')
 
     # create report
     report = GraphReport(name=report_name)
-    report.add_properties([model_v1, model_v2])
+    report.add_properties([model_cls.name() for model_cls in model_clss])
     for g, g_name in get_graphs_in_store(**kwargs):
         report.add_graph_data(g, g_name)
-        write_header(log=log_res_v1, g=g, g_name=g_name)
-        report.add_property_values_from_function(
-            p_name=model_v1, f=solve_bc_v1, g=g, log_gb=log_gb_v1, log_res=log_res_v1, time_limit=120)
-        write_header(log=log_res_v2, g=g, g_name=g_name)
-        report.add_property_values_from_function(
-            p_name=model_v2, f=solve_bc_v2, g=g, log_gb=log_gb_v2, log_res=log_res_v2, time_limit=120)
+        for model_cls in model_clss:
+            model = model_cls(g=g, g_name=g_name, dir_logs=dir_ts_logs)
+            report.add_property_values_from_function(p_name=model_cls.name(), f=model.solve)
     # save report
     report.save_csv()
-    # close files
-    log_gb_v1.close()
-    log_gb_v2.close()
-    log_res_v1.close()
-    log_res_v2.close()
 
 
 if __name__ == '__main__':
     create_and_save_model_comparison_report(
-        report_name='complete-multipartite-comparison', fname_regex='complete.*partite')
+        report_name='simple', fname_regex='simple', model_clss=[ModelV1, ModelV2])
